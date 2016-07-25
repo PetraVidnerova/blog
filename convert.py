df49c8341bf95558d5792b46500e3dfe6cda522d
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-07-25 16:08:16 +0200

    autopep

--- convert.py
+++ convert.py
@@ -1,35 +1,40 @@
-import os.path, time
+import os.path
+import time
 import blog
 
-source="ril_export.html" 
-print("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />")
-print("<!-- Source:",source+str(time.ctime(os.path.getmtime(source))),"-->")
+source = "ril_export.html"
+print(
+    "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />")
+print("<!-- Source:", source +
+      str(time.ctime(os.path.getmtime(source))), "-->")
 
 
-items=[] 
+items = []
 for line in open(source):
     line = line.strip()
     if "<li>" in line:
-        items.append(blog.item(line)) 
+        items.append(blog.item(line))
 
-tags = [ "veda", "skolstvi", "vyzvy", "akce", 
-         "ai", "kyber", "robotika", "genetika",
-         "mozek", "klima", "pocasi", "tools",
-         "ruzne" ] 
+tags = ["veda", "skolstvi", "vyzvy", "akce",
+        "ai", "kyber", "robotika", "genetika",
+        "mozek", "klima", "pocasi", "tools",
+        "ruzne"]
 
-names = [ "Věda", "Školství", "Výzvy", "Akce",
-          "Umělá inteligence", "Kybernetická bezpečnost", 
-          "Robotika", "Genetika",
-          "Mozek", "Klimatologie", "Počasí", "Užitečné nástroje",
-          "Různé" ] 
+names = ["Věda", "Školství", "Výzvy", "Akce",
+         "Umělá inteligence", "Kybernetická bezpečnost",
+         "Robotika", "Genetika",
+         "Mozek", "Klimatologie", "Počasí", "Užitečné nástroje",
+         "Různé"]
 
 for t in range(len(tags)):
-    print("<!--",tags[t],"-->")
-    tag_items = [ x for x in items if x.tag == tags[t] ] 
+
+    print("<!--", tags[t], "-->")
+    tag_items = [x for x in items if x.tag == tags[t]]
     if not tag_items:
         print("<!-- dneska nic :( -->")
     else:
-        print("<b>",names[t],"</b>")
+        print("<b>", names[t], "</b>")
         print("<ul>")
-        for i in tag_items: print(i) 
+        for i in tag_items:
+            print(i)
         print("</ul>")
