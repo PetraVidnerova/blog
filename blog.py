5842f3188617fd725f84f8fb75a263bf070072a6
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-08-01 16:33:52 +0200

    making the code more pretty

--- blog.py
+++ blog.py
@@ -21,14 +21,34 @@ with open("del_strings.txt", encoding="utf-8") as delfile:
 
 class item:
 
-    def short_source(self, href):
-        # Go through sources and return first match.
+    def __init__(self, line):
+        """ Extract href, title, tag and source. """
+
+        self.href = re.findall(r'href="(.*?)"', line)[0]
+        self.title = re.findall(r'<a href=.*>(.*?)</a>', line)[0]
+
+        if "http" in self.title or self.title == "undefined":
+            self.make_title()
+
+        self.reduce_title()
+
+        self.tag = re.findall(r' tags="(.*?)"', line)[0]
+        self.src = self.short_source()
+
+
+
+    def short_source(self):
+        """ Go through sources and return first match. """
+
         for source, abbrev in SOURCES:
-            if re.search(source, href):
+            if re.search(source, self.href):
                 return abbrev
         return("?")
 
+
     def make_title(self):
+        """ Download the webpage and extract title. """
+
         print("Downloading", self.href, file=sys.stderr)
         try:
             webpage = url.urlopen(self.href).read() 
@@ -42,18 +62,13 @@ class item:
         except:
             self.title = "no title"
 
+
     def reduce_title(self):
+        """ Delete the extra stuff from title. """
+
         for s in DEL_STRINGS:
             self.title = self.title.replace(s, "")
-            
-    def __init__(self, line):
-        self.href = re.findall("href=\"(.*?)\"", line)[0]
-        self.title = re.findall("<a href=.*>(.*?)</a>", line)[0]
-        if "http" in self.title or self.title == "undefined":
-            self.make_title()
-        self.reduce_title()
-        self.tag = re.findall("\ tags=\"(.*?)\"", line)[0]
-        self.src = self.short_source(self.href)
+
 
     def __str__(self):
         return '<li><a href=\"' + self.href + '\">' + self.title + '</a> <small>(' + self.src + ')</small></li>'
